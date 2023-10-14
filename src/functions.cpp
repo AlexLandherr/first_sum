0b2c5070d6836a535773e3f0717d44b07596af35
Alex Landherr <alex.landherr@gmail.com>
2023-10-14 11:05:01 +0000

    Added 'txt_file_to_vector()' function and tested it via output to std::cout.

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,7 +1,30 @@
 #include "include/functions.h"
+#include <vector>
+#include <cstdint>
+#include <string>
+#include <fstream>
+#include <algorithm>
 
 /*
 Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 */
 
-namespace func {}
+namespace func {
+    std::vector<std::string> txt_file_to_vector(std::string file_path) {
+        std::vector<std::string> grid;
+        std::fstream grid_file(file_path);
+        std::string single_line;
+
+        while (std::getline(grid_file, single_line)) {
+            //Remove irrelevant chars from line.
+            //single_line.erase(std::remove(single_line.begin(), single_line.end(), ' '), single_line.cend());
+            single_line.erase(std::remove(single_line.begin(), single_line.end(), '\n'), single_line.cend());
+            single_line.erase(std::remove(single_line.begin(), single_line.end(), '\r'), single_line.cend());
+
+            //Add grid_line to grid vector.
+            grid.push_back(single_line);
+        }
+
+        return grid;
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include "include/functions.h"
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
 
 /*
 Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 */
 
 int main() {
+    auto vec = func::txt_file_to_vector("prob13.txt");
+    for (std::string row : vec) {
+        std::cout << row << '\n';
+    }
 
     return 0;
 }
